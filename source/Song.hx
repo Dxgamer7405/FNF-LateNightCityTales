db67d8f3794c2910c13baed939da3ba39a5fca10
Dxgamer7405 <95672691+Dxgamer7405@users.noreply.github.com>
2023-07-11 13:51:08 -0400

    update Song.hx

--- source/Song.hx
+++ source/Song.hx
@@ -98,7 +98,7 @@ class Song
 		#if MODS_ALLOWED
 		var moddyFile:String = Paths.modsJson(formattedFolder + '/' + formattedSong);
 		if(Util.exists(moddyFile)) {
-			rawJson = File.getContent(moddyFile).trim();
+			rawJson = Util.getContent(moddyFile).trim();
 		}
 		#end
 
